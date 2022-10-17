5a5b4fb7f82fc0ec8c368dd57c64fbcc26128536
190103479 <190103479@stu.sdu.edu.kz>
2022-10-17 12:24:04 +0600

    last changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ function Navbar() {
 
 const scrollWithOffsetSchedule= (el) => {
   const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-  const yOffset = -80; 
+  const yOffset = -120; 
   window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
 }
 
